9d093f2437fbef411f20669a40db862f863f73d0
M-Morsy <mohamed.morsey96@gmail.com>
2021-05-15 13:40:56 +0200

    inital commit

---
+++ linkedin-clone/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Login from "./components/Login"
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login>
+              
+            </Login>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
