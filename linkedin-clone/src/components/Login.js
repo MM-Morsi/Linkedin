fcfb4399f3a89da1da9aae099214f1870303298d
M-Morsy <mohamed.morsey96@gmail.com>
2021-05-15 17:17:17 +0200

    added text and hero image to the first section in login page

--- linkedin-clone/src/components/Login.js
+++ linkedin-clone/src/components/Login.js
@@ -20,6 +20,12 @@ const Login = (props) => {
                     </SignIn>
                 </div>
             </Nav>
+            <Section>
+                <Hero>
+                    <h1>Welcome to your professional community</h1>
+                    <img src="/images/login-hero.svg"></img>
+                </Hero>
+            </Section>
         </Container>
     );
 }
@@ -82,6 +88,57 @@ const SignIn = styled.a`
         text-decoration: none;
         cursor: pointer;
     }
+`;
 
+const Section = styled.div`
+    display: flex;
+    align-content: flex-start; // for flex containers - comment and the align elements gets everything in middle
+    min-height: 700px;
+    padding-bottom: 138px;
+    padding-top: 40px;
+    padding: 60px 0;
+    position: relative;
+    flex-wrap: wrap;    // wrap the image under the text @ small screens
+    width: 100%;
+    max-width: 1128px;
+    align-items: center;
+    margin: auto; // center the elements inside the container automatically
+    @media (max-width: 768px){
+        margin: auto;
+        min-height: 0px;
+    }
+`
+
+const Hero = styled.div`
+    width: 100%;
+    h1 {
+        padding-bottom: 0;
+        width: 55%;
+        font-size: 56px;
+        color: #2977c9;
+        font-weight: 200;
+        line-height: 70px;
+        @media (max-width: 768px){
+            text-align: center;
+            font-size: 20px;
+            width: 100%;
+            line-height: 2;
+        }
+    }
+
+    img {
+        // z-index: -1;
+        width: 700px;
+        height: 670px;
+        position: absolute;
+        bottom: -2px;
+        right: -150px;
+        @media (max-width: 768px){
+            top: 230px;
+            width: initial;
+            position: initial;
+            height: initial;
+        }
+    }
 `;
 export default Login;
