b037ec937783ec388942b104fd5c5ca0d3465306
M-Morsy <mohamed.morsey96@gmail.com>
2021-05-15 14:52:07 +0200

    comments and notes added for first coding session

--- linkedin-clone/src/components/Login.js
+++ linkedin-clone/src/components/Login.js
@@ -1,10 +1,15 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+
+/* arrow function here for the FUNCTION component  */ 
+/* here we return the html >> JSX really we are writing in javascript files */
+/* props here is an input: https://reactjs.org/docs/components-and-props.html */
 const Login = (props) => {
+    /* return <div> Login </div> */
     return(
         <Container>
             <Nav>
                 <a href="/"> 
-                    <img src="images/login-logo.svg" alt="Logo"></img> 
+                    <img src="/images/login-logo.svg" alt="Logo"></img> 
                 </a>
                 <div>
                     <Join>
@@ -17,6 +22,7 @@ const Login = (props) => {
 }
 
 // add styling
+// without this we get an error : 'Container' is not defined.
 const Container = styled.div`
     padding: 0px;
 `;
@@ -30,15 +36,19 @@ const Nav = styled.nav`
     justify-content: space-between;
     flex-wrap: nowrap;
     & > a {
+        // background-color: red;
         width: 135px;
         height: 34px;
         @media (max-width: 768px){
-            padding: 0 5px;
+            padding: 0 5px; // top and bottom: 0px & right and left: 5px
         }
     }
 `;
 
 const Join = styled.a`
+    color: grey;
+    font-weight: 800;   
+    
 `;
 
 export default Login;
